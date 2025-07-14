8c6a5c067e83a39e18bc780a5526578fb3ffc4ec
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-07-14 20:59:56 +0200

    first basic project done

--- react-js-coding-challenge/src/App.jsx
+++ react-js-coding-challenge/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  //Exercise 1: Hello World!
+  // Every programming language starter tutorial teaches you how to output
+  // "Hello, World!" using the language.
+  // But this exercise is not just for tradition.
+  // This will asses if you actually know HTML, which is one of the many prerequisites
+  //  before learning React. Start by rendering a square with a background color.
+  // Inside the square, render "Hello, World!".
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="box">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
-
-export default App
