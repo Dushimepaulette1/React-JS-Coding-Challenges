5a52216764020930271647dce95e6d7d198ee5cd
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-07-14 22:26:14 +0200

    question 4 using useState done

--- react-js-coding-challenge/src/App.jsx
+++ react-js-coding-challenge/src/App.jsx
@@ -1,4 +1,5 @@
 import UserClicks from "./components/Button";
+import React from "react";
 export default function App() {
   //Exercise 1: Hello World!
   // Every programming language starter tutorial teaches you how to output
@@ -7,8 +8,12 @@ export default function App() {
   // This will asses if you actually know HTML, which is one of the many prerequisites
   //  before learning React. Start by rendering a square with a background color.
   // Inside the square, render "Hello, World!".
+  let [count, setCount] = React.useState(0);
   function handleClick(buttons) {
-    alert(`${buttons} clicked`);
+    // Exercise 4: State and Props
+    // Use the useState React hook to track how many times a button is clicked, and display the number.
+    // The number must increment each time the button is clicked:
+    setCount((prevCount) => prevCount + 1);
   }
 
   return (
@@ -16,7 +21,9 @@ export default function App() {
       <div className="box">
         <h1>Hello World</h1>
       </div>
-      <UserClicks OnClick={() => handleClick("Button 1")}> Button 1</UserClicks>
+      <UserClicks OnClick={() => handleClick("Button 1")} count={count}>
+        Button 1
+      </UserClicks>
       <UserClicks OnClick={() => handleClick("Button 2")}> Button 2</UserClicks>
       <UserClicks OnClick={() => handleClick("Button 3")}> Button 3</UserClicks>
     </>
--- react-js-coding-challenge/src/components/Button.jsx
+++ react-js-coding-challenge/src/components/Button.jsx
@@ -9,21 +9,23 @@ export default function UserClicks(props) {
   // Using the native HTML <button> tag, capture the click event and alert the message, "Clicked!".
   // Take note that capturing events such as clicks is done differently in React than it is in JavaScript.
 
-
-// In the previous exercise, we used the HTML <button> tag. But much of the power that React provides
-// to us developers is being able to create our own components and reuse them.
-// You'll be creating more complicated custom components than a simple button 
-// in the near future, but we all gotta start somewhere.
-// In this exercise, build your own Button component and render it three times. 
-// On user click, it should alert which button was clicked:
-// The custom component should accept an onClick prop. On render, it should 
-// render the children betwen the button. This is the text you want for the button.
-// Think of it like the HTML <button> tag. Whatever text you place between the opening and closing tag is 
-// rendered between the button when you inspect the element in your browser developer console.
-// On user click, it should alert which button was clicked.
+  // In the previous exercise, we used the HTML <button> tag. But much of the power that React provides
+  // to us developers is being able to create our own components and reuse them.
+  // You'll be creating more complicated custom components than a simple button
+  // in the near future, but we all gotta start somewhere.
+  // In this exercise, build your own Button component and render it three times.
+  // On user click, it should alert which button was clicked:
+  // The custom component should accept an onClick prop. On render, it should
+  // render the children betwen the button. This is the text you want for the button.
+  // Think of it like the HTML <button> tag. Whatever text you place between the opening and closing tag is
+  // rendered between the button when you inspect the element in your browser developer console.
+  // On user click, it should alert which button was clicked.
   return (
-    <button onClick={props.OnClick} className="clickMe">
-      {props.children}
-    </button>
+    <>
+      <h3>Button was Clicked: {props.count} </h3>
+      <button onClick={props.OnClick} className="clickMe">
+        {props.children}
+      </button>
+    </>
   );
 }
